97b0e96c06dded78f58a57def68ef6c43d7a41f1
Ben Chen <idben.chen@gmail.com>
2026-03-24 10:20:43 +0800

    express test

--- websocket01/index.js
+++ websocket01/index.js
@@ -1,1 +1,16 @@
-console.log("QQ");
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+
+app.get("/", (req, res) => {
+    res.send("Hello Server");
+});
+
+
+
+app.listen(PORT, () => {
+    console.log(`SERVER有開了 http://localhost:${PORT}`);
+
+});
